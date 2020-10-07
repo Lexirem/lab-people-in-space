71c53249d543f77099c6794bccd2d76a99dfec15
Lexirem <me.vallribera@gmail.com>
2020-10-07 17:56:15 +0200

    iter 1 i 2

--- async-wait.js
+++ async-wait.js
@@ -7,14 +7,38 @@
 const astroUrl = "http://api.open-notify.org/astros.json";
 const wikiUrl = "https://en.wikipedia.org/api/rest_v1/page/summary/";
 
-function getProfiles() {
+async function getProfiles() {
   // Aquí escribiremos la función que hará las llamadas a las API
+  const astro = await fetch(astroUrl);
+  const astrojson = await astro.json();
+  let wiki = [];
+  for (let i=0; i<astrojson.people.length; i++){
+    const person= await fetch(wikiUrl + astrojson.people[i].name);
+    const wikijson = await person.json();
+    wiki[i] = wikijson;
+  };
+  let allinformation = {
+    astro: astrojson,
+    wiki: wiki
+  };
+  return allinformation;
 }
+getProfiles();
 
 // Iteración 2
 
-function generateHTML() {
+async function generateHTML() {
   // Aquí escribiremos la función que generará los elementos HTML que mostraremos
+  let people = document.getElementById('people');
+  let allInformation = await getProfiles();
+  for (let i=0; i<allinformation.astro.people.length; i++){
+    let sectionTag = document.createElement('section');
+    sectionTag.innerHTML = `<span>${allinformation.astro.people[2].craft} </span>
+    <h2> <nombre-de-la-persona> </h2><img src="${allinformation.wiki[2].thumbnail.source}"/>
+    <p> <descripción> </p><p> <extract> /p>`;
+    people.appendChild(sectionTag);       
+  }
+
 }
 
 // Iteración 3
